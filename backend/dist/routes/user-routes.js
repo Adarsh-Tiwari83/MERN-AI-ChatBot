05337d4299f28dfb9b2765c0b840d5ccc90e85a0
Adarsh Tiwari <tadarsh249@gmail.com>
2024-01-21 17:03:16 +0530

    bug fixes

--- backend/dist/routes/user-routes.js
+++ backend/dist/routes/user-routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { getAllUser, UsersignUp } from "../controllers/user-controller.js";
 const userRoutes = Router();
-userRoutes.get("/", getAllUsers);
-userRoutes.post("/signup", userSignup);
+userRoutes.get("/", getAllUser);
+userRoutes.post("/signup", UsersignUp);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map
