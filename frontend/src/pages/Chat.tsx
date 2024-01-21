b8a533fcbfaaa6961aa956a11dfc2dfec99de7e1
Aman Raj <amanraj25100811@gmail.com>
2024-01-21 23:59:26 +0530

    verified jwt token and integrated open ai to node js

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,11 +1,117 @@
-import React from 'react'
+import React ,{useRef,useState} from 'react';
+import {Box,Avatar,Typography,Button,IconButton} from "@mui/material";
+import red from '@mui/material/colors/red';
+import { useAuth } from '../context/AuthContext';
+import ChatItem from '../components/chat/chatItem';
+import { IoMdSend } from "react-icons/io";
+import {
+  deleteUserChats,
+  getUserChats,
+  sendChatRequest,
+} from "../helpers/api-communicator";
 
+type Message = {
+  role: "user" | "assistant";
+  content: string;
+};
 const Chat = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const inputRef=useRef<HTMLInputElement | null>(null);
+  const auth=useAuth();
+  const [chatMessages, setChatMessages] = useState<Message[]>([]);
+  const handleSubmit = async () => {
+    const content = inputRef.current?.value as string;
+    if (inputRef && inputRef.current) {
+      inputRef.current.value = "";
+    }
+    const newMessage: Message = { role: "user", content };
+    setChatMessages((prev) => [...prev, newMessage]);
+    const chatData = await sendChatRequest(content);
+    setChatMessages([...chatData.chats]);
+    //
+  };
+  return <Box 
+  sx={{
+    display:'flex',
+    flex: 1,  
+    width: "100%",
+    height: "100%",
+    mt: 3,
+    gap: 3,
+  }}
+  >
+    <Box sx={{display: {md: "flex",xs:"none",sm:"none" },flex:0.2,flexDirection:'column'}}>
+    <Box sx={{
+      display: "flex", 
+      width: "100%",
+      height: "60vh",
+      bgcolor:"rgb(17,29,39",
+      borderRadius:5,
+      flexDirection:"column",
+      mx: 3,
+  }}
+  >
+    <Avatar
+    sx={{
+      mx: "auto",
+      my: 2,
+      bgcolor:"white",
+      color:"black",
+      fontWeight:700,
+    }}>
+        {auth?.user?.name[0]}
+        {auth?.user?.name.split(" ")[1][0]}
+    </Avatar>
+    <Typography sx={{mx: 'auto', fontFamily:"work sans"}}>
+      You are talking to a ChatBot
+    </Typography>
+    <Typography sx={{mx: 'auto', fontFamily:"work sans",my:4, p: 3}}>
+      You can ask anything related to knowledge,Business,Education,etc.
+      Avoid sharing personal information
+    </Typography>
+    <Button sx={{
+    width: "200px",
+    my: 'auto',
+    color:'white',
+    fontWeight:"700",
+    borderRadius:3,
+    mx:'auto',
+    bgcolor:red[300],
+    ":hover":{
+      bgcolor: red.A400,
+    },
+    }}>Clear Conversation</Button>
+  </Box>
+    </Box>
+    <Box sx={{display: "flex", flex:{md : 0.8,xs: 1,sm:1},width:"100%"}}>
+      <Typography
+        sx={{fontize:"40px",color: "white",mb:2,mx: "auto",fontWeight: "600",}}
+      >
+        model - GPT AdarshAtulAman Turbo
+      </Typography>
+      <Box sx={{width:"100%",
+      height:"60vh",
+      borderRadius:3,
+      mx:'auto',
+      display:'flex',
+      flexDirection:"column",
+      overflowX: "hidden",
+      overflowY:"auto",
+      scrollBehaviour:"smooth" ,
+      }}
+      >{chatMessages.map((chat,index)=>(
+        //@ts-ignore
+         <ChatItem content={chat.content} role={chat.role} key={index}/>
+      ))}
+      </Box>
+      <div style={{width:"100%",padding:"20px",borderRadius:8,backgroundColor:"rgb(17,27,39)",display:"flex",margin:"auto"}}></div>
+
+      <input ref={inputRef}type="text" style={{width: "100%",backgroundColor:"transparent",padding:'10px',border:"none",outline:"none",color:"white",background:"20px"}}/>
+      <IconButton onClick={handleSubmit} sx={{ color: "white", mx: 1 }}>
+      <IoMdSend />
+          </IconButton>
+
+    </Box>
+  </Box>
+};
 
 export default Chat
