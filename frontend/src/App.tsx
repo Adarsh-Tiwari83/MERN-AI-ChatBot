f16713fc1c83963a21f043cd68854e2a401223dd
Adarsh Tiwari <tadarsh249@gmail.com>
2024-01-22 22:31:27 +0530

    Added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
+import Footer from "./components/Header"
 import Home from "./pages/Home"
 import Chat from "./pages/Chat"
 import Login from "./pages/Login"
@@ -7,18 +8,19 @@ import Signup from "./pages/Signup"
 import NotFound from "./pages/NotFound"
 
 function App() {
-  
-  
+
+
   return (
     <main>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/chat" element={<Chat/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="*" element={<NotFound/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
+      <Footer />
     </main>
   )
 }
