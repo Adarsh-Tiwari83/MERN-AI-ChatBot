5c02015fa75a19d951496f7ba8a2f1f1e25cc205
Adarsh-Tiwari83 <97396071+Adarsh-Tiwari83@users.noreply.github.com>
2024-01-26 16:33:41 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,12 @@ app.use("/api/v1", appRouter);
 
 app.use(express.static(path.join(__dirname,'../../frontend/dist')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../../frontend/dist/index.html'));
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'../../frontend/dist/index.html'));
+// })
+
+app.get('/',(req,res)=>{
+    res.send('Hello guys');
+    
 })
 export default app;
