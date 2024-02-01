f7b4388de8021a3932e5fded4c5d37df494e129f
Adarsh-Tiwari83 <97396071+Adarsh-Tiwari83@users.noreply.github.com>
2024-02-01 16:57:12 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,13 +10,15 @@ const app = express();
 import appRouter from "./routes/index.js";
 
 //middlewares
-app.use(cors({ origin: "https://mern-ai-chat-bot-3qjx.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://mern-ai-chat-bot-3qjx.vercel.app",methods:["GET","POST"],credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //remove it in production
 app.use(morgan("dev"));
-
+app.get('/',(req,res)=>{
+  res.json("HELLO");
+})
 app.use("/api/v1", appRouter);
 
 
